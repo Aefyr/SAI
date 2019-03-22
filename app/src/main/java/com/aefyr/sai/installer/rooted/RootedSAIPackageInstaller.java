d8bcead9692752340c0c2da8c85fafffebaaee5b
Aefyr <mcpetergorx@gmail.com>
2019-03-22 19:53:05 +0300

    *rooted installer now retrieves installed app package

--- app/src/main/java/com/aefyr/sai/installer/rooted/RootedSAIPackageInstaller.java
+++ app/src/main/java/com/aefyr/sai/installer/rooted/RootedSAIPackageInstaller.java
@@ -1,17 +1,23 @@
 package com.aefyr.sai.installer.rooted;
 
 import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageInfo;
 import android.util.Log;
 
+import com.aefyr.sai.BuildConfig;
 import com.aefyr.sai.R;
 import com.aefyr.sai.installer.SAIPackageInstaller;
 import com.aefyr.sai.model.apksource.ApkSource;
 import com.aefyr.sai.utils.Root;
+import com.crashlytics.android.Crashlytics;
 
 import java.io.File;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -21,6 +27,33 @@ public class RootedSAIPackageInstaller extends SAIPackageInstaller {
     @SuppressLint("StaticFieldLeak")//This is application context, lul
     private static RootedSAIPackageInstaller sInstance;
 
+    private AtomicBoolean mIsAwaitingBroadcast = new AtomicBoolean(false);
+
+    private BroadcastReceiver mPackageInstalledBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d(TAG, intent.toString());
+            if (!mIsAwaitingBroadcast.get())
+                return;
+
+            String installedPackage = "null";
+            try {
+                installedPackage = intent.getDataString().replace("package:", "");
+                String installerPackage = getContext().getPackageManager().getInstallerPackageName(installedPackage);
+                Log.d(TAG, "installerPackage=" + installerPackage);
+                if (!installerPackage.equals(BuildConfig.APPLICATION_ID))
+                    return;
+            } catch (Exception e) {
+                Crashlytics.logException(e);
+                Log.wtf(TAG, e);
+            }
+
+            mIsAwaitingBroadcast.set(false);
+            dispatchCurrentSessionUpdate(InstallationStatus.INSTALLATION_SUCCEED, installedPackage);
+            installationCompleted();
+        }
+    };
+
     public static RootedSAIPackageInstaller getInstance(Context c) {
         return sInstance != null ? sInstance : new RootedSAIPackageInstaller(c);
     }
@@ -28,6 +61,9 @@ public class RootedSAIPackageInstaller extends SAIPackageInstaller {
     private RootedSAIPackageInstaller(Context c) {
         super(c);
         sInstance = this;
+        IntentFilter packageAddedFilter = new IntentFilter(Intent.ACTION_PACKAGE_ADDED);
+        packageAddedFilter.addDataScheme("package");
+        getContext().registerReceiver(mPackageInstalledBroadcastReceiver, packageAddedFilter);
     }
 
     @SuppressLint("DefaultLocale")
@@ -41,7 +77,7 @@ public class RootedSAIPackageInstaller extends SAIPackageInstaller {
                 return;
             }
 
-            String result = ensureCommandSucceeded(Root.exec("pm install-create -r"));
+            String result = ensureCommandSucceeded(Root.exec(String.format("pm install-create -r -i %s", BuildConfig.APPLICATION_ID)));
             Pattern sessionIdPattern = Pattern.compile("(\\d+)");
             Matcher sessionIdMatcher = sessionIdPattern.matcher(result);
             sessionIdMatcher.find();
@@ -50,14 +86,13 @@ public class RootedSAIPackageInstaller extends SAIPackageInstaller {
             while (apkSource.nextApk())
                 ensureCommandSucceeded(Root.exec(String.format("pm install-write -S %d %d \"%s\"", apkSource.getApkLength(), sessionId, apkSource.getApkName()), apkSource.openApkInputStream()));
 
-
-            result = ensureCommandSucceeded(Root.exec(String.format("pm install-commit %d ", sessionId)));
-            if (result.toLowerCase().contains("success"))
-                dispatchCurrentSessionUpdate(InstallationStatus.INSTALLATION_SUCCEED, "null");
-            else
-                dispatchCurrentSessionUpdate(InstallationStatus.INSTALLATION_FAILED, getContext().getString(R.string.installer_error_root, result));
-
-            installationCompleted();
+            mIsAwaitingBroadcast.set(true);
+            Root.Result installationResult = Root.exec(String.format("pm install-commit %d ", sessionId));
+            if (!installationResult.isSuccessful()) {
+                mIsAwaitingBroadcast.set(false);
+                dispatchCurrentSessionUpdate(InstallationStatus.INSTALLATION_FAILED, getContext().getString(R.string.installer_error_root, installationResult.toString()));
+                installationCompleted();
+            }
         } catch (Exception e) {
             Log.w(TAG, e);
             dispatchCurrentSessionUpdate(InstallationStatus.INSTALLATION_FAILED, getContext().getString(R.string.installer_error_root, e.getMessage()));
