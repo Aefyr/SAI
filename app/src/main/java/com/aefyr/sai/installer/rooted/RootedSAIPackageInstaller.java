68d61cc36f2856f838eba8cb1e8c5a6cd946a9a1
Aefyr <mcpetergorx@gmail.com>
2019-08-25 00:03:58 +0300

    make rooted installer compatible with some devices update gradle increment version

--- app/src/main/java/com/aefyr/sai/installer/rooted/RootedSAIPackageInstaller.java
+++ app/src/main/java/com/aefyr/sai/installer/rooted/RootedSAIPackageInstaller.java
@@ -8,6 +8,7 @@ import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.util.Log;
+import android.util.Pair;
 
 import com.aefyr.sai.BuildConfig;
 import com.aefyr.sai.R;
@@ -17,12 +18,17 @@ import com.aefyr.sai.utils.Logs;
 import com.aefyr.sai.utils.Root;
 import com.aefyr.sai.utils.Utils;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RootedSAIPackageInstaller extends SAIPackageInstaller {
     private static final String TAG = "RootedSAIPI";
+
+    private static final String COMMAND_CREATE_SESSION_NORMAL = "pm install-create -r --install-location 0 -i %s";
+    private static final String COMMAND_CREATE_SESSION_LITE = "pm install-create -r -i %s";
 
     @SuppressLint("StaticFieldLeak")//This is application context, lul
     private static RootedSAIPackageInstaller sInstance;
@@ -77,11 +83,7 @@ public class RootedSAIPackageInstaller extends SAIPackageInstaller {
                 return;
             }
 
-            String result = ensureCommandSucceeded(Root.exec(String.format("pm install-create -r --install-location 0 -i %s", BuildConfig.APPLICATION_ID)));
-            Pattern sessionIdPattern = Pattern.compile("(\\d+)");
-            Matcher sessionIdMatcher = sessionIdPattern.matcher(result);
-            sessionIdMatcher.find();
-            int sessionId = Integer.parseInt(sessionIdMatcher.group(1));
+            int sessionId = createSession();
 
             while (apkSource.nextApk())
                 ensureCommandSucceeded(Root.exec(String.format("pm install-write -S %d %d \"%s\"", apkSource.getApkLength(), sessionId, apkSource.getApkName()), apkSource.openApkInputStream()));
@@ -114,5 +116,47 @@ public class RootedSAIPackageInstaller extends SAIPackageInstaller {
             Log.wtf(TAG, "Unable to get SAI version", e);
         }
         return String.format("%s: %s %s | %s | Android %s | Using %s ApkSource implementation | SAI %s", getContext().getString(R.string.installer_device), Build.BRAND, Build.MODEL, Utils.isMiui() ? "MIUI" : "Not MIUI", Build.VERSION.RELEASE, apkSource.getClass().getSimpleName(), saiVersion);
+    }
+
+    private int createSession() throws RuntimeException {
+        ArrayList<String> commandsToAttempt = new ArrayList<>();
+        commandsToAttempt.add(String.format(COMMAND_CREATE_SESSION_NORMAL, BuildConfig.APPLICATION_ID));
+        commandsToAttempt.add(String.format(COMMAND_CREATE_SESSION_LITE, BuildConfig.APPLICATION_ID));
+
+        List<Pair<String, String>> attemptedCommands = new ArrayList<>();
+
+        for (String commandToAttempt : commandsToAttempt) {
+            String result = ensureCommandSucceeded(Root.exec(commandToAttempt));
+            attemptedCommands.add(new Pair<>(commandToAttempt, result));
+
+            Integer sessionId = extractSessionId(result);
+            if (sessionId != null)
+                return sessionId;
+            else
+                Log.w(TAG, String.format("Command failed: %s > %s", commandToAttempt, result));
+        }
+
+        StringBuilder exceptionMessage = new StringBuilder("Unable to create session, attempted commands: ");
+        for (Pair<String, String> attemptedCommand : attemptedCommands) {
+            exceptionMessage.append("\n\n==========================\n")
+                    .append(attemptedCommand.first)
+                    .append("\nVVVVVVVVVVVVVVVV\n")
+                    .append(attemptedCommand.second);
+        }
+        exceptionMessage.append("\n");
+
+        throw new IllegalStateException(exceptionMessage.toString());
+    }
+
+    private Integer extractSessionId(String commandResult) {
+        try {
+            Pattern sessionIdPattern = Pattern.compile("(\\d+)");
+            Matcher sessionIdMatcher = sessionIdPattern.matcher(commandResult);
+            sessionIdMatcher.find();
+            return Integer.parseInt(sessionIdMatcher.group(1));
+        } catch (Exception e) {
+            Log.w(TAG, commandResult, e);
+            return null;
+        }
     }
 }
