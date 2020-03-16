a2d6de6466e57deb172eecbb1a5fffa05414ef0b
Aefyr <mcpetergorx@gmail.com>
2020-03-16 21:08:32 +0300

    add modes to ThemeSelectionDialogFragment will be required later to choose themes for dark/light modes

--- app/src/main/java/com/aefyr/sai/ui/dialogs/ThemeSelectionDialogFragment.java
+++ app/src/main/java/com/aefyr/sai/ui/dialogs/ThemeSelectionDialogFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.IntDef;
 import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
@@ -18,19 +19,49 @@ import com.aefyr.sai.billing.DonationStatus;
 import com.aefyr.sai.ui.activities.DonateActivity;
 import com.aefyr.sai.ui.dialogs.base.BaseBottomSheetDialogFragment;
 import com.aefyr.sai.utils.Theme;
+import com.aefyr.sai.utils.Utils;
 
 public class ThemeSelectionDialogFragment extends BaseBottomSheetDialogFragment implements ThemeAdapter.OnThemeInteractionListener {
 
+    @IntDef(flag = true, value = {MODE_APPLY, MODE_CHOOSE})
+    public @interface Mode {
+    }
+
+    public static final int MODE_APPLY = 0;
+    public static final int MODE_CHOOSE = 1;
+
+    private static final String EXTRA_MODE = "mode";
+
     private BillingManager mBillingManager;
 
+    private int mMode = MODE_APPLY;
+
+    /**
+     * Same as {@link #newInstance(int)} with MODE_APPLY
+     */
     public static ThemeSelectionDialogFragment newInstance(Context context) {
+        return newInstance(MODE_APPLY);
+    }
+
+    public static ThemeSelectionDialogFragment newInstance(@Mode int mode) {
         ThemeSelectionDialogFragment fragment = new ThemeSelectionDialogFragment();
+
+        Bundle args = new Bundle();
+        args.putInt(EXTRA_MODE, mode);
+
+        fragment.setArguments(args);
+
         return fragment;
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Bundle args = getArguments();
+        if (args != null) {
+            mMode = args.getInt(EXTRA_MODE, MODE_APPLY);
+        }
 
         mBillingManager = DefaultBillingManager.getInstance(requireContext());
     }
@@ -68,11 +99,29 @@ public class ThemeSelectionDialogFragment extends BaseBottomSheetDialogFragment 
         DonationStatus donationStatus = mBillingManager.getDonationStatus().getValue();
         if (theme.isDonationRequired() && !(donationStatus == DonationStatus.DONATED || donationStatus == DonationStatus.FLOSS_MODE)) {
             DonateActivity.start(requireContext());
-        } else {
-            Theme.getInstance(getContext()).setCurrentTheme(theme);
-            requireActivity().recreate();
-            dismiss();
+            return;
         }
+
+        switch (mMode) {
+            case MODE_APPLY:
+                Theme.getInstance(getContext()).setCurrentTheme(theme);
+                requireActivity().recreate();
+                dismiss();
+                break;
+            case MODE_CHOOSE:
+                OnThemeChosenListener listener = Utils.getParentAs(this, OnThemeChosenListener.class);
+                if (listener != null)
+                    listener.onThemeChosen(theme);
+
+                dismiss();
+                break;
+            default:
+                throw new IllegalStateException("Unknown mode");
+        }
+    }
+
+    public interface OnThemeChosenListener {
+        void onThemeChosen(Theme.ThemeDescriptor theme);
     }
 }
 
