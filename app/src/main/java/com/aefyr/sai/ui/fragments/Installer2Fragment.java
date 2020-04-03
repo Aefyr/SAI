77e2bf19a8679ba77da433c31f7c27d8777ca1c1
Aefyr <mcpetergorx@gmail.com>
2020-04-04 00:48:17 +0300

    allow selection of xapk files

--- app/src/main/java/com/aefyr/sai/ui/fragments/Installer2Fragment.java
+++ app/src/main/java/com/aefyr/sai/ui/fragments/Installer2Fragment.java
@@ -196,7 +196,7 @@ public class Installer2Fragment extends InstallerFragment implements FilePickerD
         properties.selection_type = DialogConfigs.FILE_SELECT;
         properties.root = Environment.getExternalStorageDirectory();
         properties.offset = new File(mHelper.getHomeDirectory());
-        properties.extensions = new String[]{"apk", "zip", "apks"};
+        properties.extensions = new String[]{"apk", "zip", "apks", "xapk"};
         properties.sortBy = mHelper.getFilePickerSortBy();
         properties.sortOrder = mHelper.getFilePickerSortOrder();
 
@@ -264,7 +264,7 @@ public class Installer2Fragment extends InstallerFragment implements FilePickerD
 
         String extension = Utils.getExtension(files.get(0).getName());
 
-        if (".apks".equals(extension) || ".zip".equals(extension)) {
+        if (".apks".equals(extension) || ".zip".equals(extension) || ".xapk".equals(extension)) {
             mViewModel.installPackagesFromZip(files);
         } else if (".apk".equals(extension)) {
             mViewModel.installPackages(files);
