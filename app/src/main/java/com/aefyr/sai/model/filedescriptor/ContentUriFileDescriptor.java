20285673ce9b13ad4e0f6a846f7488714ff733be
Aefyr <mcpetergorx@gmail.com>
2020-04-04 01:24:11 +0300

    fix ContentUriFileDescriptor not working with file uris

--- app/src/main/java/com/aefyr/sai/model/filedescriptor/ContentUriFileDescriptor.java
+++ app/src/main/java/com/aefyr/sai/model/filedescriptor/ContentUriFileDescriptor.java
@@ -2,9 +2,11 @@ package com.aefyr.sai.model.filedescriptor;
 
 import android.content.ContentResolver;
 import android.content.Context;
-import android.database.Cursor;
 import android.net.Uri;
-import android.provider.MediaStore;
+
+import androidx.documentfile.provider.DocumentFile;
+
+import com.aefyr.sai.utils.saf.SafUtils;
 
 import java.io.InputStream;
 
@@ -12,43 +14,32 @@ public class ContentUriFileDescriptor implements FileDescriptor {
 
     private ContentResolver mContentResolver;
     private Uri mContentUri;
+    private DocumentFile mDocumentFile;
 
     public ContentUriFileDescriptor(Context c, Uri contentUri) {
         mContentResolver = c.getContentResolver();
         mContentUri = contentUri;
+        mDocumentFile = SafUtils.docFileFromSingleUriOrFileUri(c, contentUri);
     }
 
 
     @Override
     public String name() throws Exception {
-        try (Cursor cursor = mContentResolver.query(mContentUri, new String[]{MediaStore.MediaColumns.DISPLAY_NAME}, null, null, null)) {
-            if (cursor == null)
-                throw new BadContentProviderException("Cursor is null");
+        String name = mDocumentFile.getName();
+        if (name == null)
+            throw new BadContentProviderException("DISPLAY_NAME column is null");
 
-            cursor.moveToFirst();
-            String name = cursor.getString(0);
-
-            if (name == null)
-                throw new BadContentProviderException("DISPLAY_NAME column is null");
-
-            return name;
-        }
+        return name;
     }
 
     @Override
     public long length() throws Exception {
-        try (Cursor cursor = mContentResolver.query(mContentUri, new String[]{MediaStore.MediaColumns.SIZE}, null, null, null)) {
-            if (cursor == null)
-                throw new BadContentProviderException("Cursor is null");
+        long length = mDocumentFile.length();
 
-            cursor.moveToFirst();
-            long length = cursor.getLong(0);
+        if (length == 0)
+            throw new BadContentProviderException("SIZE column is 0");
 
-            if (length == 0)
-                throw new BadContentProviderException("SIZE column is 0");
-
-            return length;
-        }
+        return length;
     }
 
     @Override
