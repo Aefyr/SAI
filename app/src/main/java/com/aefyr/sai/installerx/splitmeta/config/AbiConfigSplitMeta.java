53ab5f20a74e76cdccd567a24ebf24a49f23a0a8
Aefyr <mcpetergorx@gmail.com>
2020-06-08 18:21:14 +0300

    support more archs in installerx parser

--- app/src/main/java/com/aefyr/sai/installerx/splitmeta/config/AbiConfigSplitMeta.java
+++ app/src/main/java/com/aefyr/sai/installerx/splitmeta/config/AbiConfigSplitMeta.java
@@ -10,18 +10,24 @@ import java.util.Set;
 
 public class AbiConfigSplitMeta extends ConfigSplitMeta {
 
+    public static final String ARMEABI = "armeabi";
     public static final String ARMEABI_V7A = "armeabi_v7a";
     public static final String ARM64_V8A = "arm64_v8a";
     public static final String X86 = "x86";
     public static final String X86_64 = "x86_64";
+    public static final String MIPS = "mips";
+    public static final String MIPS64 = "mips64";
 
     private static Set<String> ALL_ABIS = new HashSet<>();
 
     static {
+        ALL_ABIS.add(ARMEABI);
         ALL_ABIS.add(ARMEABI_V7A);
         ALL_ABIS.add(ARM64_V8A);
         ALL_ABIS.add(X86);
         ALL_ABIS.add(X86_64);
+        ALL_ABIS.add(MIPS);
+        ALL_ABIS.add(MIPS64);
     }
 
     private String mAbi;
